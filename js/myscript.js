8d469912a82270c74fa705cd44afb4f2f3911244
daviddpi <piscopodavid97@gmail.com>
2021-08-24 18:58:50 +0200

    cambiati variabili e sistemato if

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,11 @@
 var chilometriPercorrere = parseInt( prompt("Inserisci i km da percorrere") );
 var etaPasseggero = parseInt( prompt("Inserisci l'età del passegero") );
 
-var centisimiAlKm = 0.21;
+const centisimiAlKm = 0.21;
 var prezzoTotale = centisimiAlKm * chilometriPercorrere;
 
-var scontoAnziani= (prezzoTotale * 40) / 100;
-var scontoGiovani= (prezzoTotale  *20) / 100;
+const scontoAnziani= (prezzoTotale * 40) / 100;
+const scontoGiovani= (prezzoTotale  *20) / 100;
 
 console.log("Età passegero:" + etaPasseggero);
 console.log("Chilometri da percorrere:" + chilometriPercorrere);
@@ -19,7 +19,7 @@ if(etaPasseggero==Number.isNaN){
     alert("Errore, deve essere un numero!")
 }
 
-if(etaPasseggero >= 65){
+if(etaPasseggero > 65){
     prezzoFinale = prezzoTotale - scontoAnziani
 } else if(etaPasseggero < 18){
     prezzoFinale = prezzoTotale - scontoGiovani;
